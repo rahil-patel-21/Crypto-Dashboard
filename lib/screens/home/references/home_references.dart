329762c5b2cb3cb149144938583b22637182a1dc
TechAddict <rahil.geek21@outlook.com>
2021-03-16 00:50:58 +0530

    added referecnes to pattern

---
+++ lib/screens/home/references/home_references.dart
@@ -0,0 +1,9 @@
+//Custom Widgets
+export 'package:webstabledemo/screens/home/ui/widgets/side_tab_button.dart';
+
+//Flutter Imports
+export 'package:flutter/material.dart';
+
+//Local Imports
+export 'package:webstabledemo/utilities/constants/strings.dart';
+export 'package:webstabledemo/utilities/theme/app_theme.dart';
